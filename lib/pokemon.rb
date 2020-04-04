ac4fc39b0cbc8a1ca56881d8b82cb9d83078b7e7
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-04 01:01:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
+require 'pry'
+
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db 
+  
+  @@all = []
+  
+  def initialize(arguments)
+    @id = arguments[:id]
+    @name = arguments[:name]
+    @type = arguments[:type]
+    @db = arguments[:db]
+    
+  end 
+  
+  def self.save(name, type, db) 
+   
+  end
+  
 end
